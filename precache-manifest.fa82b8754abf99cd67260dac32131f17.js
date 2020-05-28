f74748b2c78f677597e3efafc86b3caaa9397097
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-05-28 12:34:27 +0200

    Updates

---
+++ precache-manifest.fa82b8754abf99cd67260dac32131f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82559fa47902084af5f37d328012e0fc",
+    "url": "/meme_generator_react/index.html"
+  },
+  {
+    "revision": "f62546e9de3a42aebbe6",
+    "url": "/meme_generator_react/static/css/main.a1275bab.chunk.css"
+  },
+  {
+    "revision": "99c047dadd3d10b14f68",
+    "url": "/meme_generator_react/static/js/2.609c28b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme_generator_react/static/js/2.609c28b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62546e9de3a42aebbe6",
+    "url": "/meme_generator_react/static/js/main.1e793383.chunk.js"
+  },
+  {
+    "revision": "3ed7160999693b86e907",
+    "url": "/meme_generator_react/static/js/runtime-main.db008db7.js"
+  }
+]);
